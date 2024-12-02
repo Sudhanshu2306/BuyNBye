a190b02a56f64a64a01234229500048ba1bc00bc
ujjwalagrawal-1 <kumarujjwalagrawal4@gmail.com>
2024-12-02 20:14:54 +0530

    Some New Files added

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); // To hash the password
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -12,13 +13,24 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        required: [true, "Please Enter Gender"]
+        required: [true, "Please Enter Gender"],
     },
     password: {
         type: String,
         required: [true, "Please Enter Your Password"],
-        minLength: [8, "Password should have atleast 8 chars"],
-        select: false,
+        minLength: [8, "Password should have at least 8 characters"],
+        select: false, // Prevent the password from being returned in queries
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, "Please Confirm Your Password"],
+        validate: {
+            // Ensure confirmPassword matches password
+            validator: function (value) {
+                return value === this.password;
+            },
+            message: "Passwords do not match",
+        },
     },
     designation: {
         type: String,
@@ -28,6 +40,22 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+});
+
+// Pre-save middleware to hash the password before saving to the database
+userSchema.pre('save', async function (next) {
+    // Only run this function if password is modified
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    // Remove confirmPassword field after validation
+    this.confirmPassword = undefined;
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
