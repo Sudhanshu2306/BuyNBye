66b086a219a7a7361f725acf363b54168a0fd0be
Sudhanshu Raj <sudhanshuraj2306@gmail.com>
2024-12-07 02:44:04 +0530

    sell page template set

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Card from './components/Card'
 import About from './pages/AboutUs'
+import Sell from './Sell/Sell'
 function App() {
   return (
     <div>
        <Navbar></Navbar>
        {/* <Card></Card> */}
-       <About></About>
+       {/* <About></About> */}
+       <Sell></Sell>
        <Footer></Footer>
     </div>
   )
